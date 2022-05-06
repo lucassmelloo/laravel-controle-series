10aed2097f2d23483ea02f5b2ed1abb0bbc992dc
Lucas de Mello Vieira <lucas.vieira@ntl.labchecap.com.br>
2022-05-06 16:33:37 -0300

    Criando GitSearch

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\GithubSearch;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeriesController;
 
@@ -13,3 +13,5 @@ Route::get('/', function () {
 Route::get('/series',[SeriesController::class, 'index'] );
 
 Route::get('/series/criar',[SeriesController::class, 'create'] );
+
+Route::get('/githubsearch',[GithubSearch::class, 'index']);
