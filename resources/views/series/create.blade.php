c77b9f49312037dc39a6fb90aa2d6eb305117c7e
Mello <lucasmellovieira99@gmail.com>
2022-05-06 21:27:34 -0300

    Adicionando Bootstrap

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,7 +1,10 @@
 <x-layout title="Nova Serie">
-<form action="" methot="post">
-    <label for="nome">Nome:</label>
-    <input type="text" id="nome" name="nome">
-</form>
-
+    <form action="" methot="post">
+    <div class="mb-3">
+        <label for="nome" class="form-label">Nome:</label>
+        <input type="text" id="nome" name="nome" class="form-control">
+    </div>
+    <button class="btn btn-primary" type="submit">Adicionar</button>
+    <a class="btn btn-primary" href="/series">Retornar</a>
+    </form>
 </x-layout>
